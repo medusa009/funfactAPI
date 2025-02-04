f3bfcfd7a82246480df674e30c768e9520b7ca54
Razak Quadri <razak.quadri@decagon.dev>
2025-02-04 09:50:26 +0100

    Docker added

--- HNG-Stage-One/obj/Debug/net8.0/HNG-Stage-One.AssemblyInfo.cs
+++ HNG-Stage-One/obj/Debug/net8.0/HNG-Stage-One.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HNG-Stage-One")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e727e5dc3c6af420d5f3cd1392cd854f8f2a62e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HNG-Stage-One")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HNG-Stage-One")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
